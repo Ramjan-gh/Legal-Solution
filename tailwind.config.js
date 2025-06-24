b97f80f2c37fc0b8015984f546cf4d0d650961ea
ramjan-gh <ramzanhridoy@gmail.com>
2025-06-25 02:26:20 +0600

    stressed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      
-    },
-  },
-  plugins: [],
-};
+
+ /** @type {import('tailwindcss').Config} */
+ export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+       colors: {
+         "btn-color": "#E76F51",
+         "primary-bg": "rgba(182,140,90,1)",
+       },
+       fontFamily: {
+         poppins: ["Poppins", "sans-serif"],
+         eb: ["EB Garamond", "serif"],
+       },
+     },
+   },
+   plugins: [],
+ };
